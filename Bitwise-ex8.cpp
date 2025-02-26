d3b48070128b0919e671783e460e42579032835a
Alexander V Wiedman <113028724+AlexWiedman@users.noreply.github.com>
2025-02-26 18:55:57 +0000

    Ex8

--- Bitwise-ex8.cpp
+++ Bitwise-ex8.cpp
@@ -1,1 +1,13 @@
-// Your code here
+#include <iostream>
+#include <bitset>
+
+int main(){
+    std::bitset<8> b{0b01110010};
+    std::cout << b << " (initial value)\n";
+
+    for(int i = 0; i < b.size(); i++){
+        b.flip(i);
+        std::cout << b << '\n';
+    }
+    std::cout << b << " (final value)\n";
+}
