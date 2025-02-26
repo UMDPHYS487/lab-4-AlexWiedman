ccfc2bdb7ed8cd89d338209e7c1ba92f6711bcca
Alexander V Wiedman <113028724+AlexWiedman@users.noreply.github.com>
2025-02-26 18:24:46 +0000

    Ex5

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,17 @@
-// Add your code here
+#include <iostream>
+
+int factorial(int a){
+    int i = 1;
+    for(int j = 1; j < a+1; j++){
+        i = i * j;
+    }
+    return i;
+}
+
+
+int main(){
+    int n;
+    std::cout << "Please enter an integer value: ";
+    std::cin >> n;
+    std::cout << "The value you entered is " << n << " and its factorial is " << factorial(n) << "\n";
+}
