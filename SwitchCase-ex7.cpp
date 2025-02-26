b946c5896737394c768596569096aee5ca611277
Alexander V Wiedman <113028724+AlexWiedman@users.noreply.github.com>
2025-02-26 19:12:18 +0000

    Ex7 fix2

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -4,8 +4,8 @@ int main(){
     int n;
     std::cout << "Please enter an integer value: ";
     std::cin >> n;
-    std::cout << !(n > 10 && n % 2 != 0) << '\n';
-    switch (n % 2 + 2 * !(n > 10 && n % 2 != 0))
+    std::cout << (n > 10 && n % 2 == 0) << '\n';
+    switch (n % 2 + 2 * (n > 10 && n % 2 == 0))
     {
     case 0:
         std::cout << "Your number is " << n << " and its square is " << n*n << "\n";
